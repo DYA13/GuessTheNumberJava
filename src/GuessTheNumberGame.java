e6263ca83c0e29ffd6dc078c41142ceee7a2ade0
Yelena Japarova <yelena.dzhaparova@gmail.com>
2025-06-14 17:46:15 +0100

    main functions created

---
+++ src/GuessTheNumberGame.java
@@ -0,0 +1,18 @@
+public class GuessTheNumberGame {
+    public static void main(String[] args) {
+        //start and play again functionality
+        System.out.println("Hello world!");
+    }
+    public static int chooseDifficulty(){
+// max number and max attempts
+    }
+    public static void playGame(int maxNumber,int maxAttempts){
+        //Main game loop
+    }
+    public static void giveHint(int guess, int target){
+        //Hint functionality caldo/freddo
+    }
+    public static boolean askToPlayAgain(){
+        //ask if user wants to play again
+    }
+}
