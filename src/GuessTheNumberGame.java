d8d89e8fbb3a38b7c39f986b6e37f95be60d975f
Yelena Japarova <yelena.dzhaparova@gmail.com>
2025-06-16 09:44:29 +0100

    Add initial structure for Guess the Number game

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -1,9 +1,39 @@
+import java.util.Scanner; 
+import java.util.Random;
+
+
 public class GuessTheNumberGame {
     public static void main(String[] args) {
-        //start and play again functionality
-        System.out.println("Hello world!");
+       Scanner scanner= new Scanner(System.in);
+
+       boolean playAgain;
+       System.out.println("Welcome to Guess the Number Game!");
+       
+       do{
+        int difficulty = chooseDifficulty(scanner);
+        int maxNumber =0;
+        int maxAttempts =0;
+
+        if (difficulty ==1){
+            maxNumber= 10;
+            maxAttempts=5;
+        } else if(difficulty ==2){
+            maxNumber=50;
+            maxAttempts= 7;
+        } else {
+            maxNumber= 100;
+            maxAttempts= 10;
+        }
+        playGame(scanner,maxNumber,maxAttempts);
+        playAgain =askToPlayAgain();
+
+       } while (playAgain);
+       System.out.println("Thank you for a game! Goodbye!");
+       scanner.close();
+    
     }
     public static int chooseDifficulty(){
+        return 0;
 // max number and max attempts
     }
     public static void playGame(int maxNumber,int maxAttempts){
@@ -13,6 +43,7 @@ public class GuessTheNumberGame {
         //Hint functionality caldo/freddo
     }
     public static boolean askToPlayAgain(){
+        return true;
         //ask if user wants to play again
     }
 }
